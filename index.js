83f1bd31e7a148cc8e62798d405bca49f1ae04aa
Jana Veliskova <veliskova@gmail.com>
2024-10-15 22:39:32 -0400

    load freelancers

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
-// === State ===
-// Here, we define variables for the data that our program needs to remember.
-// We call this data "state" because it represents the state of our program.
-// This is also where we define functions to modify the state.
 
-// TODO: Add support for more colors
-const colors = ["red", "green", "blue"];
-const sizes = ["small", "medium", "large"];
-const maxShapes = 10;
-const shapes = [
-  {
-    color: "red",
-    size: "small",
-  },
-  {
-    color: "yellow",
-    size: "small",
-  },
+const freelancers = [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+  { name: "Dr. Cool", price: 85, occupation: "cook" },
+  { name: "Ms. Anthropy", price: 33, occupation: "professional friend" }
 ];
 
-/** Adds a shape with random properties to the `shapes` array */
-function addShape() {
-  const color = colors[Math.floor(Math.random() * colors.length)];
 
-  // TODO: Randomize the size of the shape
-  const size = "small";
+/** Adds a freelancer to the list of freelancers. */
+function addFreelancer() {  
+  // Get a random number between 0 and the number of freelancers
+  const randomIndex = Math.floor(Math.random() * freelancers.length);
+  let randomFreelancer = freelancers.splice(randomIndex, 1)[0];
 
-  shapes.push({ color, size });
+  render(randomFreelancer);
 }
 
-// === Render ===
-// To "render" is to update the DOM to reflect the current state.
-// In this section, we define the functions to render state.
+function calculateAndRenderAveragePrice() {
+  const allPrices = document.querySelectorAll("#freelance-list p:nth-child(2)");
+  const averagePrice = document.querySelector("#average-price");
 
-/** Updates the DOM to reflect the current state. */
-function render() {
-  // Render the squares
-  const squareList = document.querySelector("#squares");
-  const squareElements = shapes.map((shape) => {
-    const squareElement = document.createElement("li");
-    squareElement.classList.add(shape.color, shape.size);
-    return squareElement;
-  });
-  squareList.replaceChildren(...squareElements);
+  
 
-  // TODO: Render the circles
+
 }
 
-// === Script ===
-// In this section, we call the functions that we've defined above.
+// Render the freelancers
+function render(person) {
+  const freelancerUl = document.querySelector("#freelance-list");
+  const template = document.querySelector("#freelancer");
+  const clone = template.content.cloneNode(true);
 
-// `setInterval` will call the callback function every 1000 milliseconds (1 second)
-// and return an interval ID that we can use to stop the interval later.
-// Calling `clearInterval(addShapeIntervalId)` will stop the interval.
-const addShapeIntervalId = setInterval(() => {
-  addShape();
-  render();
+  let h2 = clone.querySelector("h2");
+  let p = clone.querySelectorAll("p");
 
-  // TODO: Stop adding shapes if we've reached the maximum number of shapes
+  if (person) {
+    h2.textContent = person.name;
+    p[0].textContent = person.occupation;
+    p[1].textContent = `Price: $${person.price}`;
+  }
+
+  freelancerUl.appendChild(clone);
+  calculateAndRenderAveragePrice();
+}
+
+
+const addFreelancerInterval = setInterval(() => {
+  addFreelancer();
+
+  // stop adding freelancers if we have reached the max
+  if (freelancers.length === 0) {
+    clearInterval(addFreelancerInterval);
+  }
 }, 1000);
 
 render(); // We call this function once to render the initial state
